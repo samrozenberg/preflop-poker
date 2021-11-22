57484fb8fd5f30d1fc0a21922ba9fbea485770b7
Sam Rozenberg <sam.rozen@hotmail.com>
2021-11-23 00:12:28 +0100

    changes complete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 
 
-import { joinGame } from "../game_features/join_game"
-joinGame()
+// import { joinGame } from "../game_features/join_game"
+// joinGame()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
