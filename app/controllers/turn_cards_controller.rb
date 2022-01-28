c7c178751c48206b5b0b90b6b73fd9fd5fc6b205
Sam Rozenberg <sam.rozen@hotmail.com>
2022-01-28 18:07:51 +0100

    improvement game show complete

--- app/controllers/turn_cards_controller.rb
+++ app/controllers/turn_cards_controller.rb
@@ -2,6 +2,10 @@ class TurnCardsController < ApplicationController
   def create
     @game = Game.find(params[:game_id])
     @hand = @game.hands.last
+    @in_hand_players = []
+    UserHand.where(hand: @hand, active: true).each do |userhand|
+      @in_hand_players << userhand.user
+    end
     @used_cards = []
     @game.hands.last.user_cards.each do |user_card|
       @used_cards << user_card.deck_card
@@ -11,6 +15,22 @@ class TurnCardsController < ApplicationController
     end
     @available_cards = DeckCard.all - @used_cards
     TurnCard.create(hand: @hand, deck_card: @available_cards.sample)
+
+    UserHand.where(user: @in_hand_players, hand: @hand).each do |user_hand|
+      card_combination = []
+      UserCard.where(user: user_hand.user, hand: @hand).each do |card|
+        card_combination << card.deck_card.code
+      end
+      FlopCard.where(hand: @hand).each do |card|
+        card_combination << card.deck_card.code
+      end
+      TurnCard.where(hand: @hand).each do |card|
+        card_combination << card.deck_card.code
+      end
+      pokerhand = PokerHand.new(card_combination)
+      user_hand.update_attribute(:rank, pokerhand.rank)
+    end
+
     redirect_to game_path(@game)
   end
 end
