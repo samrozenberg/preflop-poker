dcaa2395312882061a6c3761ae4f84f21d9aad6e
Sam Rozenberg <sam.rozen@hotmail.com>
2022-02-07 19:34:39 +0100

    odd calculation after turn complete

--- app/controllers/flop_cards_controller.rb
+++ app/controllers/flop_cards_controller.rb
@@ -24,34 +24,40 @@ class FlopCardsController < ApplicationController
 
     @current_hand = @game.hands.last
 
-    @players_card_combinations = []
     @in_hand_players.each do |player|
       instance_variable_set("@#{player.pseudo}_cards", [])
       UserCard.where(hand: @current_hand, user: player).each do |card|
         instance_variable_get("@#{player.pseudo}_cards") << card.deck_card.code
       end
-      @players_card_combinations << instance_variable_get("@#{player.pseudo}_cards")
     end
 
     @winners = []
-
-    if @current_hand.river_cards.count == 0 && @current_hand
-      @combinations.each do |combination|
-        winning_simulation = PokerHand.new("")
-        winning_player = @in_hand_players[0]
-        @in_hand_players.each do |player|
-          instance_variable_set("@#{player.pseudo}_pokerhand", PokerHand.new(instance_variable_get("@#{player.pseudo}_cards")))
-          combination.each do |card|
-            instance_variable_get("@#{player.pseudo}_pokerhand") << card.code
-          end
-          if instance_variable_get("@#{player.pseudo}_pokerhand") > winning_simulation
-            winning_simulation = instance_variable_get("@#{player.pseudo}_pokerhand")
-            winning_player = player
-          end
+    @flop_cards_codes = []
+    @flop_cards.each do |card|
+      @flop_cards_codes << card.code
+    end
+    @combinations.each do |combination|
+      winning_simulation = PokerHand.new("")
+      winning_player = []
+      @in_hand_players.each do |player|
+        instance_variable_set("@#{player.pseudo}_pokerhand", PokerHand.new(instance_variable_get("@#{player.pseudo}_cards")))
+        combination.each do |card|
+          instance_variable_get("@#{player.pseudo}_pokerhand") << card.code
         end
-        @winners << winning_player
+        instance_variable_get("@#{player.pseudo}_pokerhand") << @flop_cards_codes
+        if instance_variable_get("@#{player.pseudo}_pokerhand") > winning_simulation
+          winning_simulation = instance_variable_get("@#{player.pseudo}_pokerhand")
+          winning_player.clear
+          winning_player << player
+        elsif instance_variable_get("@#{player.pseudo}_pokerhand") == winning_simulation
+          winning_player << player
+        end
+      end
+      winning_player.each do |player|
+        @winners << player
       end
     end
+
 
     UserHand.where(user: @in_hand_players, hand: @hand).each do |user_hand|
       card_combination = []
@@ -63,7 +69,7 @@ class FlopCardsController < ApplicationController
       end
       pokerhand = PokerHand.new(card_combination)
       user_hand.update_attribute(:rank, pokerhand.rank)
-      odd = "#{(@winners.count(user_hand.user) / @combinations.count.to_f * 100).round(0)}%"
+      odd = (@winners.count(user_hand.user) / @combinations.count.to_f * 100).round(1) % 1 == 0 ? "#{(@winners.count(user_hand.user) / @combinations.count.to_f * 100).round(1).to_i}%" : "#{(@winners.count(user_hand.user) / @combinations.count.to_f * 100).round(1)}%"
       user_hand.update_attribute(:odds, odd)
     end
 
