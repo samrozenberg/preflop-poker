b9570b31478f960e0148cd88945e6929f35b392e
Sam Rozenberg <sam.rozen@hotmail.com>
2022-02-07 13:42:33 +0100

    changes complete

--- app/controllers/flop_cards_controller.rb
+++ app/controllers/flop_cards_controller.rb
@@ -15,6 +15,44 @@ class FlopCardsController < ApplicationController
       FlopCard.create(hand: @hand, deck_card: card)
     end
 
+    @flop_cards = []
+    @hand.flop_cards.each do |card|
+      @flop_cards << card.deck_card
+    end
+    @combination_available_cards = @available_cards - @flop_cards
+    @combinations = @combination_available_cards.combination(2).to_a
+
+    @current_hand = @game.hands.last
+
+    @players_card_combinations = []
+    @in_hand_players.each do |player|
+      instance_variable_set("@#{player.pseudo}_cards", [])
+      UserCard.where(hand: @current_hand, user: player).each do |card|
+        instance_variable_get("@#{player.pseudo}_cards") << card.deck_card.code
+      end
+      @players_card_combinations << instance_variable_get("@#{player.pseudo}_cards")
+    end
+
+    @winners = []
+
+    if @current_hand.river_cards.count == 0 && @current_hand
+      @combinations.each do |combination|
+        winning_simulation = PokerHand.new("")
+        winning_player = @in_hand_players[0]
+        @in_hand_players.each do |player|
+          instance_variable_set("@#{player.pseudo}_pokerhand", PokerHand.new(instance_variable_get("@#{player.pseudo}_cards")))
+          combination.each do |card|
+            instance_variable_get("@#{player.pseudo}_pokerhand") << card.code
+          end
+          if instance_variable_get("@#{player.pseudo}_pokerhand") > winning_simulation
+            winning_simulation = instance_variable_get("@#{player.pseudo}_pokerhand")
+            winning_player = player
+          end
+        end
+        @winners << winning_player
+      end
+    end
+
     UserHand.where(user: @in_hand_players, hand: @hand).each do |user_hand|
       card_combination = []
       UserCard.where(user: user_hand.user, hand: @hand).each do |card|
@@ -25,6 +63,8 @@ class FlopCardsController < ApplicationController
       end
       pokerhand = PokerHand.new(card_combination)
       user_hand.update_attribute(:rank, pokerhand.rank)
+      odd = (@winners.count(user_hand.user)) / @combination.count
+      raise
     end
 
     redirect_to game_path(@game)
