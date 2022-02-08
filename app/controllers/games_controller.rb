52ab920ce16da0c96fcd404f0779965eefdb5765
Sam Rozenberg <sam.rozen@hotmail.com>
2022-02-08 19:24:12 +0100

    style improvement complete

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,7 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.all
+    @games = Game.all.reverse
+    @poker_url = ["https://worldfinancialreview.com/wp-content/uploads/2021/04/flush-royal-in-poker-player-hand-lucky-winner-stockpack-adobe-stock-scaled-1.jpg", "https://images.theconversation.com/files/213908/original/file-20180409-114076-691a0s.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop", "https://www.mypokercoaching.com/wp-content/uploads/2020/04/playing-poker-for-a-living.jpg", "https://elie.net/static/images/banner/fuller-house-exposing-high-end-poker-cheating-devices.jpg", "https://www.mypokercoaching.com/wp-content/uploads/2020/04/playing-poker-for-a-living.jpg", "https://pnimg.net/w/articles/0/5dd/fc5534810a.jpg", "https://pnimg.net/w/articles/4/59d/4e06f9bdf7.jpg", "https://i.ytimg.com/vi/K4P3DI6cqxo/maxresdefault.jpg", "https://st4.depositphotos.com/13194036/i/600/depositphotos_308141268-stock-photo-handsome-man-smoking-cigar-black.jpg", "https://media.istockphoto.com/photos/man-playing-texas-hold-em-poker-at-casino-picture-id1158580119?k=20&m=1158580119&s=612x612&w=0&h=BBJ1K591BGmiEPzXJ8v9i8O6RpyxcjWN_HJ0j0wtTnY=", "https://thumbs.dreamstime.com/b/random-poker-cards-over-green-background-169032439.jpg", "https://thesalonhost.com/wp-content/uploads/2021/08/1_6NrFpflyvQ3WzklkfBvUgQ-1.jpg", "https://www.filthylimper.com/wp-content/uploads/2017/03/random-poker-thoughts-2.jpg", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/US_Navy_090620-N-2798F-033_Sailors_assigned_to_the_aircraft_carrier_USS_Harry_S._Truman_%28CVN_75%29_and_Carrier_Air_Wing_%28CVW%29_3_compete_in_a_Texas_Hold_%27Em_Poker_tournament_aboard_Harry_S._Truman.jpg/800px-thumbnail.jpg", "https://asset.montecarlosbm.com/styles/hero_desktop_wide_responsive/s3/media/orphea/ca_poker_2018_0001_0.jpg.jpeg?itok=HBpEFAG-", "https://casino.giocodigitale.it/it/blog/wp-content/uploads/2020/05/poker-per-principianti-1145x620.jpg", "https://www.ilpost.it/wp-content/uploads/2019/07/poker.jpg", "https://kaxmedia.com/cdn-cgi/image/h=384,w=680/https://objects.kaxmedia.com/auto/o/1870/6ddefbd693.png", "https://media.nature.com/lw800/magazine-assets/d41586-019-02156-9/d41586-019-02156-9_16920180.jpg", "https://yourpokerdealer.com/wp-content/uploads/2019/02/dealer.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiSznKsAL3SurVWi19TXPq7bKsvetUfoW7eQ&usqp=CAU", "https://thebankrollers.com/wp-content/uploads/2014/12/poker.jpg", "https://ccaf-khmer.org/wp-content/uploads/2021/01/2-2.jpg", "https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2013/11/26/1385479353669/98ae8297-8cf7-4138-8a44-09f4573625ec-2060x1236.jpeg?width=620&quality=85&auto=format&fit=max&s=ba797a92287c9dee573b76fce7098bc5", "https://store-images.s-microsoft.com/image/apps.32865.13672061214767198.3336fb4c-c283-4579-ad82-96f02585b57c.9e2e868f-e342-4328-af5d-411155672ccb?mode=scale&q=90&h=1080&w=1920", "https://i.insider.com/5f3aa40ee89ebf001f0453d5?width=1200&format=jpeg", "https://fundacionanade.org/wp-content/uploads/2020/08/pokerplayer.jpg"]
   end
 
   def show
@@ -46,7 +47,7 @@ class GamesController < ApplicationController
       end
     end
 
-    if @current_hand.flop_cards.count == 3
+    if @current_hand && @current_hand.flop_cards.count == 3
       @current_winners = []
       @biggest_odd = 0
       @in_hand_players.each do |player|
@@ -68,7 +69,7 @@ class GamesController < ApplicationController
   def create
     @game = Game.new(game_params)
     @game.save
-    redirect_to game_path(@game)
+    redirect_to games_path
   end
 
   def edit
