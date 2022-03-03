6c7298511f915bc26c6f33303143e58869e09e43
Sam Rozenberg <sam.rozen@hotmail.com>
2022-03-03 05:30:33 +0100

    right box complete

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -57,7 +57,6 @@ class GamesController < ApplicationController
         end
       end
     end
-
     if @current_hand && @current_hand.flop_cards.count == 3
       @current_winners = []
       @biggest_odd = 0
@@ -71,7 +70,7 @@ class GamesController < ApplicationController
         end
       end
     end
-    if @current_hand.flop_cards.count == 3 && @current_hand.name == "Pineapple"
+    if @current_hand && @current_hand.flop_cards.count == 3 && @current_hand.name == "Pineapple"
       @current_user_card_1 = UserCard.where(hand: @current_hand, user: @user)[0]
       @current_user_card_2 = UserCard.where(hand: @current_hand, user: @user)[1]
       @current_user_card_3 = UserCard.where(hand: @current_hand, user: @user)[2]
