522b77bcdd092937274bb75796f73d588b5fe49c
Sam Rozenberg <sam.rozen@hotmail.com>
2022-03-11 15:25:30 +0100

    change in user table complete

---
+++ db/migrate/20220311142038_add_hand_stats_to_users.rb
@@ -0,0 +1,8 @@
+class AddHandStatsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :users, :best_hand, foreign_key: { to_table: :hands }
+    add_reference :users, :worst_hand, foreign_key: { to_table: :hands }
+    add_column :users, :biggest_win, :integer
+    add_column :users, :biggest_loss, :integer
+  end
+end
