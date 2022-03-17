6233423537015c1144f729a76db3207b519509ee
Sam Rozenberg <sam.rozen@hotmail.com>
2022-03-18 00:33:09 +0100

    changes complete

--- app/controllers/user_hands_controller.rb
+++ app/controllers/user_hands_controller.rb
@@ -1,30 +1,31 @@
 class UserHandsController < ApplicationController
   def update
     @game = Game.find(params[:game_id])
+    @current_hand = @game.hands.last
     @user = current_user
-    @userhand = UserHand.where(hand: @game.hands.last, user: @user).last
+    @userhand = UserHand.where(hand: @current_hand, user: @user).last
     @in_hand_players = []
-    UserHand.where(hand: @game.hands.last, active: true).each do |userhand|
+    UserHand.where(hand: @current_hand, active: true).each do |userhand|
       @in_hand_players << userhand.user
     end
-    @last_better = @game.hands.last.better
+    @last_better = @current_hand.better
     if @in_hand_players.find_index(@last_better) < @in_hand_players.count - 1
       next_better_index = @in_hand_players.find_index(@last_better) + 1
     else
       next_better_index = 0
     end
     @userhand.update_attribute(:active, false)
-    @game.hands.last.update_attribute(:better, @in_hand_players[next_better_index])
+    @current_hand.update_attribute(:better, @in_hand_players[next_better_index])
 
-    if UserHand.where(hand: @game.hands.last, active: true).count == 1
+    if UserHand.where(hand: @current_hand, active: true).count == 1
       next_better_index = 500000
-      HandWinner.create(hand: @game.hands.last, winner: UserHand.where(hand: @game.hands.last, active: true)[0].user)
-      @game.hands.last.update_attribute(:better, @in_hand_players[next_better_index])
+      HandWinner.create(hand: @current_hand, winner: UserHand.where(hand: @current_hand, active: true)[0].user)
+      @current_hand.update_attribute(:better, @in_hand_players[next_better_index])
 
       total_pot = 0
-      @game.hands.last.users.each do |user|
-        if user.bets.where(hand: @game.hands.last).last
-          total_pot += user.bets.where(hand: @game.hands.last).last.amount
+      @current_hand.users.each do |user|
+        if user.bets.where(hand: @current_hand).last
+          total_pot += user.bets.where(hand: @current_hand).last.amount
         end
       end
 
@@ -32,9 +33,10 @@ class UserHandsController < ApplicationController
         total_pot += @game.hands[@game.hands.count - 2].remainder
       end
 
+
       winners = []
 
-      winners << HandWinner.where(hand: @game.hands.last, winner: UserHand.where(hand: @game.hands.last, active: true)[0].user)[0].winner
+      winners << UserHand.where(hand: @current_hand, active: true)[0].user
 
       @game.reservations.where(active: true).each do |reservation|
         resa = @game.reservations.where(user: reservation.user)[0]
